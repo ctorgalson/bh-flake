1cf536647a4ec5576da5d672b95c2d02a89750c0
Christopher Torgalson <manager@bedlamhotel.com>
2024-08-12 15:44:02 -0400

    feat: adds basic flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.ser6 = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./ser6/configuration.nix
+        # inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
