d68452f46de5f3a8440838b6629aa3496dee223b
Christopher Torgalson <manager@bedlamhotel.com>
2024-08-14 18:48:13 +0200

    feat: corrects object attribute name

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
           home-manager.nixosModules.default
 	  {
             home-manager.useGlobalPkgs = true;
-	    home-manager.useUserPkgs = true;
+	    home-manager.useUserPackages = true;
 	    home-manager.extraSpecialArgs = { inherit allowed-unfree-packages user; };
 	  }
         ];
