8cf6e3f1d48302bdec43f451d39e85d57e9a4102
Christopher Torgalson <manager@bedlamhotel.com>
2024-08-14 19:12:43 +0200

    feat: enables some programs in main configuration.nix file

--- hosts/ser6/configuration.nix
+++ hosts/ser6/configuration.nix
@@ -109,6 +109,12 @@
     vim
   ];
 
+  programs.bws.enable;
+
+  programs.steam.enable;
+
+  programs.vim.enable;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
