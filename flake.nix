c2b682a138fb37aadd5909f5a831a85a0eff36cf
Christopher Torgalson <manager@bedlamhotel.com>
2024-08-14 18:44:11 +0200

    feat: (hopefully) makes allowed-unfree-packages, user available everywhere

--- flake.nix
+++ flake.nix
@@ -10,22 +10,35 @@
     };
   };
 
-  # Home manager unfree pkgs configuration:
+  # Home manager unfree pkgs configuration, and also for general flake
+  # structure:
   # @see https://stackoverflow.com/questions/77585228/how-to-allow-unfree-packages-in-nix-for-each-situation-nixos-nix-nix-wit
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { home-manager, nixpkgs, self, ... }@inputs:
+  let
+    user = "ctorgalson";
+    allowed-unfree-packages = [
+      "bws"
+      "steam"
+    ];
+  in {
     nixosConfigurations = {
       ser6 = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
         modules = [
           ./hosts/ser6/configuration.nix
-          inputs.home-manager.nixosModules.default
+          home-manager.nixosModules.default
+	  {
+            home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPkgs = true;
+	    home-manager.extraSpecialArgs = { inherit allowed-unfree-packages user; };
+	  }
         ];
       };
       executive-14 = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
         modules = [
           ./hosts/executive-14/configuration.nix
-          inputs.home-manager.nixosModules.default
+          home-manager.nixosModules.default
         ];
       };
     };
