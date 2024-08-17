81a7d5477e36570a6c57df1ad08998604b5dec8c
Christopher Torgalson <manager@bedlamhotel.com>
2024-08-17 21:20:54 +0200

    feat: enables, minimally configures tactile gnome extension

--- modules/home-manager/dconf/default.nix
+++ modules/home-manager/dconf/default.nix
@@ -12,12 +12,22 @@
         disabled-extensions = [ ]; 
         enabled-extensions = [
           "freon@UshakovVasilii_Github.yahoo.com"
+          "tactile@lundal.io"
         ];
       };
       "org/gnome/shell/extensions/freon" = {
         show-icon-on-panel = false;
         use-drive-hddtemp = true;
         exec-method-freeipmi = "pkexec";
+      };
+      "org/gnome/shell/extensions/tactile" = {
+        row-2: 0;
+        grid-cols: 5;
+        col-4: 1;
+        layout-2-row-2: 0;
+        layout-2-col-3: 0;
+        layout-2-col-2: 0;
+        monitor-0-layout: 1;
       };
     };
   };
