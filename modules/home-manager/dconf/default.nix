e28bfbbb6e4fc7a192db0545efaedb530a0c3b85
Christopher Torgalson <manager@bedlamhotel.com>
2024-08-17 15:18:21 +0200

    feat: enable, configure freeon, enable gpaste

--- modules/home-manager/dconf/default.nix
+++ modules/home-manager/dconf/default.nix
@@ -7,6 +7,18 @@
         use-system-font = false;
         custom-font = "Inconsolata Nerd Font Mono 13";
       };
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+        disabled-extensions = [ ]; 
+        enabled-extensions = [
+          "freon@UshakovVasilii_Github.yahoo.com"
+        ];
+      };
+      "org/gnome/shell/extensions/freon" = {
+        show-icon-on-panel = false;
+        use-drive-hddtemp = true;
+        exec-method-freeipmi = "pkexec";
+      };
     };
   };
 }
---
+++ modules/home-manager/programs/gpaste.nix
@@ -0,0 +1,9 @@
+{ config, pkgs, programs, ... }:
+
+{
+  config = {
+    gpaste.atuin = {
+      enable = true;
+    };
+  };
+}
