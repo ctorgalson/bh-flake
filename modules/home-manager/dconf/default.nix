151761002e4cd2da7e526c1de5836b262d2ef965
Christopher Torgalson <manager@bedlamhotel.com>
2024-08-17 21:22:32 +0200

    feat: nix lang, not json

--- modules/home-manager/dconf/default.nix
+++ modules/home-manager/dconf/default.nix
@@ -21,13 +21,13 @@
         exec-method-freeipmi = "pkexec";
       };
       "org/gnome/shell/extensions/tactile" = {
-        row-2: 0;
-        grid-cols: 5;
-        col-4: 1;
-        layout-2-row-2: 0;
-        layout-2-col-3: 0;
-        layout-2-col-2: 0;
-        monitor-0-layout: 1;
+        row-2 = 0;
+        grid-cols = 5;
+        col-4 = 1;
+        layout-2-row-2 = 0;
+        layout-2-col-3 = 0;
+        layout-2-col-2 = 0;
+        monitor-0-layout = 1;
       };
     };
   };
