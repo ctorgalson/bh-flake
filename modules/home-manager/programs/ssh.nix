d0be1cb48237d5687b65401a3d250e3c6fc8b5bf
Christopher Torgalson <manager@bedlamhotel.com>
2024-08-18 17:40:33 +0200

    feat: adds missing host to config; adds p.sh ssh config

--- modules/home-manager/programs/ssh.nix
+++ modules/home-manager/programs/ssh.nix
@@ -4,15 +4,25 @@
   config = {
     programs.ssh = {
       enable = true;
+      extraConfig = [
+        (''
+        Host *.platform.sh
+          Include /home/ctorgalson/.platformsh/ssh/*.config
+        Host *
+        '')
+      ];
       matchBlocks = {
-      	"r2" = {
+        "r2" = {
           hostname = "135.181.200.94";
           user = "ctorgalson";
           forwardAgent = true;
-	};
+        };
+        "rcksl" = {
+          hostname = "162.55.164.32";
+          user = "ctorgalson";
+          forwardAgent = true;
+        };
       };
-      # extraConfig = [];
     };
   };
 }
-
