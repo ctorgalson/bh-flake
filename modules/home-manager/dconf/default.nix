1ab754b343c6cd6e075225196cb3f5cf04167522
Christopher Torgalson <manager@bedlamhotel.com>
2024-08-18 11:23:14 +0200

    feat: closing in on the world's most frustrating config language

--- modules/home-manager/dconf/default.nix
+++ modules/home-manager/dconf/default.nix
@@ -30,36 +30,88 @@
         monitor-0-layout = 1;
       };
       # @see https://discourse.nixos.org/t/time-zones-in-gnome-clocks-get-set-correctly-but-then-disappear-during-use/36797
-      "org/gnome/shell/clocks" = {
-        world-clocks = [
-          ([
-            (lib.hm.gvariant.mkDictionaryEntry ["location" (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
-              (lib.hm.gvariant.mkUint32 2)
-              (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
-                "Vancouver"
-                "CYVR"
-                true
-                [(lib.hm.gvariant.mkTuple [(0.85841109795478021) (-2.1496638678574467)])]
-                [(lib.hm.gvariant.mkTuple [(0.85957465660720722) (-2.1490820798045869)])]
-              ]))
-            ]))])
-          ])
+      # @see https://discourse.nixos.org/t/write-key-value-using-lib-hm-gvariant-for-home-manager/31234/4
+      # @see https://docs.gtk.org/glib/gvariant-format-strings.html
+      # "org/gnome/clocks" = {
+      #   world-clocks = [
+      #     # Lyon
+      #     ([
+      #       (lib.hm.gvariant.mkDictionaryEntry ["location" (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
+      #         (lib.hm.gvariant.mkUint32 2)
+      #         (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
+      #           "Lyon"
+      #           "LFLY"
+      #           true
+      #           [(lib.hm.gvariant.mkTuple [(0.7979063621878385) (0.086393797973719322)])]
+      #           [(lib.hm.gvariant.mkTuple [(0.79848813278740571) (0.084648468721724976)])]
+      #         ]))
+      #       ]))])
+      #     ])
+      #     # Vancouver
+      #     ([
+      #       (lib.hm.gvariant.mkDictionaryEntry ["location" (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
+      #         (lib.hm.gvariant.mkUint32 2)
+      #         (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
+      #           "Vancouver"
+      #           "CYVR"
+      #           true
+      #           [(lib.hm.gvariant.mkTuple [(0.85841109795478021) (-2.1496638678574467)])]
+      #           [(lib.hm.gvariant.mkTuple [(0.85957465660720722) (-2.1490820798045869)])]
+      #         ]))
+      #       ]))])
+      #     ])
+      #   ];
+      # };
+      "org/gnome/shell/world-clocks" = {
+        locations = [
+          # Lyon
+          (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
+            (lib.hm.gvariant.mkUint32 2)
+            (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
+              "Lyon"
+              "LFLY"
+              true
+              [(lib.hm.gvariant.mkTuple [(0.7979063621878385) (0.086393797973719322)])]
+              [(lib.hm.gvariant.mkTuple [(0.79848813278740571) (0.084648468721724976)])]
+            ]))
+          ]))
+          # Vancouver
+          (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
+            (lib.hm.gvariant.mkUint32 2)
+            (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
+              "Vancouver"
+              "CYVR"
+              true
+              [(lib.hm.gvariant.mkTuple [(0.85841109795478021) (-2.1496638678574467)])]
+              [(lib.hm.gvariant.mkTuple [(0.85957465660720722) (-2.1490820798045869)])]
+            ]))
+          ]))
         ];
-        # <(uint32 2, <('Vancouver', 'CYVR', true, [(0.85841109795478021, -2.1496638678574467)], [(0.85957465660720722, -2.1490820798045869)])>)>
       };
     };
   };
 }
 
-# Manually added
-#
 # /org/gnome/clocks/world-clocks
-#   [{'location': <(uint32 2, <('Portland', 'KPDX', true, [(0.79571014457688405, -2.1397785149603687)], [(0.7945341242735976, -2.1411037260081156)])>)>}]
+#   [{'location': <(uint32 2, <('Lyon', 'LFLY', true, [(0.7979063621878385, 0.086393797973719322)], [(0.79848813278740571, 0.084648468721724976)])>)>}]
 # 
 # /org/gnome/shell/world-clocks/locations
-#  [<(uint32 2, <('Portland', 'KPDX', true, [(0.79571014457688405, -2.1397785149603687)], [(0.7945341242735976, -2.1411037260081156)])>)>]
-#
-# From dconf in nix
-#
+#   [<(uint32 2, <('Lyon', 'LFLY', true, [(0.7979063621878385, 0.086393797973719322)], [(0.79848813278740571, 0.084648468721724976)])>)>]
+
+# /org/gnome/clocks/world-clocks
+#   [{'location': <(uint32 2, <('Dublin', 'EIDW', true, [(0.93258759116453926, -0.1090830782496456)], [(0.93083742735051689, -0.10906368764165594)])>)>}]
+# 
 # /org/gnome/shell/world-clocks/locations
-#   [{'location': <(uint32 2, <('Vancouver', 'CYVR', true, [(0.85841100000000004, -2.149664)], [(0.85957499999999998, -2.1490819999999999)])>)>}]
+#   [<(uint32 2, <('Dublin', 'EIDW', true, [(0.93258759116453926, -0.1090830782496456)], [(0.93083742735051689, -0.10906368764165594)])>)>]
+
+# /org/gnome/clocks/world-clocks
+#  [{'location': <(uint32 2, <('Toronto', 'CYTZ', true, [(0.76154532446909495, -1.3857914260834978)], [(0.76212711252195475, -1.3860823201099277)])>)>}]
+# 
+# /org/gnome/shell/world-clocks/locations
+#  [<(uint32 2, <('Toronto', 'CYTZ', true, [(0.76154532446909495, -1.3857914260834978)], [(0.76212711252195475, -1.3860823201099277)])>)>]
+
+# /org/gnome/clocks/world-clocks
+#   [{'location': <(uint32 2, <('Winnipeg', 'CYWG', true, [(0.87091929674517032, -1.6970418035380554)], [(0.87062840271874053, -1.695878262338921)])>)>}]
+# 
+# /org/gnome/shell/world-clocks/locations
+#   [<(uint32 2, <('Winnipeg', 'CYWG', true, [(0.87091929674517032, -1.6970418035380554)], [(0.87062840271874053, -1.695878262338921)])>)>]
