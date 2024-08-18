445defd480f708d6d9b2576df70774ba6bd517f7
Christopher Torgalson <manager@bedlamhotel.com>
2024-08-18 17:42:47 +0200

    feat: figure out more nix syntax :)

--- modules/home-manager/programs/ssh.nix
+++ modules/home-manager/programs/ssh.nix
@@ -4,13 +4,11 @@
   config = {
     programs.ssh = {
       enable = true;
-      extraConfig = [
-        (''
+      extraConfig = ''
         Host *.platform.sh
           Include /home/ctorgalson/.platformsh/ssh/*.config
         Host *
-        '')
-      ];
+      '';
       matchBlocks = {
         "r2" = {
           hostname = "135.181.200.94";
