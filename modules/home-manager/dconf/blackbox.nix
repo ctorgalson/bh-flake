871be3ac916db89e5510d1929152f5a8f7c1d880
Christopher Torgalson <manager@bedlamhotel.com>
2024-08-18 20:40:18 +0200

    feat: give up on padding from nix for now

--- modules/home-manager/dconf/blackbox.nix
+++ modules/home-manager/dconf/blackbox.nix
@@ -7,6 +7,10 @@
         font = "Inconsolata Nerd Font Mono 14";
         theme-bold-is-bright = true;
         theme-dark = "Solarized Dark";
+        style-preference = (lib.hm.gvariant.mkUint32 2);
+        # $%^&#$%@#& dconf
+        #
+        #terminal-padding = (lib.hm.gvariant.mkUint32 10) (lib.hm.gvariant.mkUint32 10) (lib.hm.gvariant.mkUint32 10) (lib.hm.gvariant.mkUint32 10);
       };
     };
   };
