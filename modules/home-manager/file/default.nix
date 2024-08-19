b055b92c5f81363b8ad69d39f85a3a7b63eaaffd
Christopher Torgalson <manager@bedlamhotel.com>
2024-08-19 08:56:30 +0200

    feat: ensure minimal directory structure

--- modules/home-manager/file/default.nix
+++ modules/home-manager/file/default.nix
@@ -1,7 +1,18 @@
-{ config, pkgs, home, ... }:
+{ config, lib, pkgs, home, ... }:
 
 {
   config = {
+    # Create expected directories.
+    home.activation.directories = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      dirs=( 'Dev/BH' 'Dev/AT' 'Nextcloud' )
+      for dir in "''${!fruits[@]}"; do
+        path="$HOME/$dir"
+        if [ ! -d "$path" ]; then
+          mkdir -p "$path"
+        fi
+      done
+    '';
+
     home.file = {
       # Byobu
       ".config/byobu/.tmux.conf".source = ../../../dotfiles/byobu/.tmux.conf;
