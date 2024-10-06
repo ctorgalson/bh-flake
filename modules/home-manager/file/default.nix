e128e7a85521282d17cf266e6c791c0cfaa6d89c
Christopher Torgalson <manager@bedlamhotel.com>
2024-10-06 19:27:45 +0200

    feat: back to bash for dirs, symlinks

--- modules/home-manager/file/default.nix
+++ modules/home-manager/file/default.nix
@@ -1,22 +1,16 @@
 { config, lib, pkgs, home, ... }:
 
-let
-  ansible = pkgs.ansible;
-in
 {
   config = {
     home.activation.directories = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       flake_path="/home/ctorgalson/bh-flake"
-      file_path="modules/home-manager/file/playbooks/default_directories.yml"
-      ${ansible}/bin/ansible-playbook "''${flake_path}/''${file_path}"
+      file_path="modules/home-manager/file/default_directories.sh"
+      "''${flake_path}/''${file_path}"
     '';
 
     home.file = {
       # Byobu
       ".config/byobu/.tmux.conf".source = ../../../dotfiles/byobu/.tmux.conf;
-      # Background image
-      ".background-image".source = ../../../images/.background-image;
-      ".background-image".target = ".background-image";
     };
   };
 }
