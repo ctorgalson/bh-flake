0d4d4c0313bca1c0944e9ca549f85756f1511a85
Christopher Torgalson <manager@bedlamhotel.com>
2024-10-06 19:36:18 +0200

    feat: moves bash back directly to module

--- modules/home-manager/file/default.nix
+++ modules/home-manager/file/default.nix
@@ -3,9 +3,29 @@
 {
   config = {
     home.activation.directories = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      flake_path="/home/ctorgalson/bh-flake"
-      file_path="modules/home-manager/file/default_directories.sh"
-      "''${flake_path}/''${file_path}"
+      mount_path="/run/media/''${USER}/Storage"
+      home_dirs=( 'Dev' 'Documents' 'Music' 'Nextcloud' 'Videos' )
+
+      for home_dir in "''${home_dirs[@]}"; do
+        src_path="''${mount_path}/''${home_dir}"
+        dest_path="''${HOME}/''${home_dir}"
+
+        # Ensure the source directory exists.
+        mkdir -p "''${src_path}"
+        
+        # Is this a directory and NOT a symlink?
+        if [ -d "''${dest_path}" ] && [ ! -L "''${dest_path}" ]; then
+          if [ -z "$(\ls -A "''${dest_path}")" ]; then
+            rmdir "''${dest_path}"
+          else
+            echo "Directory not empty (''${dest_path})"
+            exit 1
+          fi
+        # Is this a nonexistent directory?
+        elif [ ! -d "''${dest_path}" ]; then
+          ln -s "''${src_path}" "''${dest_path}"
+        fi
+      done
     '';
 
     home.file = {
