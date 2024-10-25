6e89f590560eabd9106635dcd1ea84366da8cf86
Christopher Torgalson <manager@bedlamhotel.com>
2024-10-25 13:38:59 +0200

    fix: adds home-manager firefox references, examples

--- modules/home-manager/programs/firefox.nix
+++ modules/home-manager/programs/firefox.nix
@@ -2,6 +2,12 @@
 
 {
   config = {
+    # References
+    #
+    # - https://github.com/Misterio77/nix-config/blob/main/home/gabriel/features/desktop/common/firefox.nix
+    # - https://home-manager-options.extranix.com/?query=firefox&release=release-24.05
+    # - https://github.com/nix-community/home-manager/blob/master/modules/programs/firefox.nix
+    # - https://discourse.nixos.org/t/declare-firefox-extensions-and-settings/36265
     programs.firefox = {
       enable = true;
 
