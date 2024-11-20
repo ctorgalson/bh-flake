ec580a4359e548e9e3c9388dd36559617b80dbce
Christopher Torgalson <manager@bedlamhotel.com>
2024-11-20 10:29:13 -0500

    feat: add large-monitor settings to tactile

--- modules/home-manager/dconf/tactile.nix
+++ modules/home-manager/dconf/tactile.nix
@@ -4,12 +4,78 @@
   config = {
     dconf.settings = {
       "org/gnome/shell/extensions/tactile" = {
+        grid-cols = 7;
+        grid-rows = 4;
+
+        # Layout 1.
+        col-0 = 1;
+        col-1 = 1;
+        col-2 = 1;
+        col-3 = 1;
+        col-4 = 0;
+        col-5 = 0;
+        col-6 = 0;
+        row-0 = 1;
+        row-1 = 1;
         row-2 = 0;
-        grid-cols = 5;
-        col-4 = 1;
-        layout-2-row-2 = 0;
-        layout-2-col-3 = 0;
-        layout-2-col-2 = 0;
+        row-3 = 0;
+
+        # Layout 2.
+        layout-2-col-0 = 1;
+        layout-2-col-1 = 1;
+        layout-2-col-2 = 1;
+        layout-2-col-3 = 1;
+        layout-2-col-4 = 1;
+        layout-2-col-5 = 1;
+        layout-2-col-6 = 1;
+        layout-2-row-0 = 1;
+        layout-2-row-1 = 1;
+        layout-2-row-2 = 1;
+        layout-2-row-3 = 1;
+
+        # Row 1.
+        tile-0-0 = "1";
+        tile-1-0 = "2";
+        tile-2-0 = "3";
+        tile-3-0 = "4";
+        tile-4-0 = "5";
+        tile-5-0 = "6";
+        tile-6-0 = "7";
+
+        # Row 2.
+        tile-0-1 = "q";
+        tile-1-1 = "w";
+        tile-2-1 = "e";
+        tile-3-1 = "r";
+        tile-4-1 = "t";
+        tile-5-1 = "y";
+        tile-6-1 = "u";
+
+        # Row 3.
+        tile-0-1 = "a";
+        tile-1-1 = "s";
+        tile-2-1 = "d";
+        tile-3-1 = "f";
+        tile-4-1 = "g";
+        tile-5-1 = "h";
+        tile-6-1 = "j";
+
+        # Row 4.
+        tile-0-1 = "z";
+        tile-1-1 = "x";
+        tile-2-1 = "c";
+        tile-3-1 = "v";
+        tile-4-1 = "b";
+        tile-5-1 = "n";
+        tile-6-1 = "m";
+        
+        # Layout keyboard shortcuts.
+        layout-1 = "F1";
+        layout-2 = "F2";
+        layout-3 = "F3";
+        layout-4 = "F4";
+        
+        # Various.
         monitor-0-layout = 1;
         background-color = "rgba(255,255,255,0.666667)";
         border-color = "rgba(0,0,0,0.333333)";
@@ -21,3 +87,22 @@
     };
   };
 }
+
+# /org/gnome/shell/extensions/tactile/layout-2-row-2
+#   1
+# /org/gnome/shell/extensions/tactile/grid-cols
+#   7
+# /org/gnome/shell/extensions/tactile/layout-2-col-6
+#   1
+# /org/gnome/shell/extensions/tactile/tile-6-0
+#   ['y']
+# /org/gnome/shell/extensions/tactile/tile-6-1
+#   ['u']
+# /org/gnome/shell/extensions/tactile/tile-6-2
+#   ['j']
+# /org/gnome/shell/extensions/tactile/tile-6-3
+#   ['m']
+# /org/gnome/shell/extensions/tactile/monitor-0-layout
+#   4
+# /org/gnome/shell/extensions/tactile/monitor-0-layout
+#   2
