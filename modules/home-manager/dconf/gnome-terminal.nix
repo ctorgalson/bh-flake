44e9ec3a9fe997c5c2bdc16721e7ba0d946c11da
Christopher Torgalson <manager@bedlamhotel.com>
2024-12-01 20:38:01 -0500

    feat: updates terminal font
    For some reason the ubuntu mono nerd font works, but the inconsolata one
    doesn't.

--- modules/home-manager/dconf/gnome-terminal.nix
+++ modules/home-manager/dconf/gnome-terminal.nix
@@ -19,7 +19,7 @@
       };
       "org/gnome/terminal/legacy/profiles:/:${config.gnome-terminal.profileId}" = {
         use-system-font = false;
-        font = "Inconsolata Nerd Font Mono 14";
+        font = "UbuntuMono Nerd Font Mono 15";
         use-theme-colors = false;
         foreground-color = "rgb(131,148,150)";
         background-color = "rgb(0,43,54)";
