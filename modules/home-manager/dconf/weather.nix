ef408111e6fa4d8e816026c3bffc08bb6686934f
Christopher Torgalson <manager@bedlamhotel.com>
2024-12-05 09:12:55 -0500

    feat: comments-out Lyon weather info in dconf/weather.nix

--- modules/home-manager/dconf/weather.nix
+++ modules/home-manager/dconf/weather.nix
@@ -5,18 +5,6 @@
     dconf.settings = {
       "org/gnome/Weather" = {
         "locations" = [
-          # Lyon
-          (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
-            (lib.hm.gvariant.mkUint32 2)
-            (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
-              "Lyon"
-              "LFLY"
-              true
-              [(lib.hm.gvariant.mkTuple [(0.7979063621878385) (0.086393797973719322)])]
-              [(lib.hm.gvariant.mkTuple [(0.79848813278740571) (0.084648468721724976)])]
-            ]))
-          ]))
-
           # Toronto
           (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
             (lib.hm.gvariant.mkUint32 2)
@@ -28,22 +16,22 @@
               [(lib.hm.gvariant.mkTuple [(0.76212711252195475) (-1.3860823201099277)])]
             ]))
           ]))
+
+          # Lyon
+          # (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
+          #   (lib.hm.gvariant.mkUint32 2)
+          #   (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
+          #     "Lyon"
+          #     "LFLY"
+          #     true
+          #     [(lib.hm.gvariant.mkTuple [(0.7979063621878385) (0.086393797973719322)])]
+          #     [(lib.hm.gvariant.mkTuple [(0.79848813278740571) (0.084648468721724976)])]
+          #   ]))
+          # ]))
         ];
       };
       "org/gnome/shell/weather" = {
         "locations" = [
-          # Lyon
-          (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
-            (lib.hm.gvariant.mkUint32 2)
-            (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
-              "Lyon"
-              "LFLY"
-              true
-              [(lib.hm.gvariant.mkTuple [(0.7979063621878385) (0.086393797973719322)])]
-              [(lib.hm.gvariant.mkTuple [(0.79848813278740571) (0.084648468721724976)])]
-            ]))
-          ]))
-
           # Toronto
           (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
             (lib.hm.gvariant.mkUint32 2)
@@ -55,6 +43,18 @@
               [(lib.hm.gvariant.mkTuple [(0.76212711252195475) (-1.3860823201099277)])]
             ]))
           ]))
+
+          # Lyon
+          # (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
+          #   (lib.hm.gvariant.mkUint32 2)
+          #   (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
+          #     "Lyon"
+          #     "LFLY"
+          #     true
+          #     [(lib.hm.gvariant.mkTuple [(0.7979063621878385) (0.086393797973719322)])]
+          #     [(lib.hm.gvariant.mkTuple [(0.79848813278740571) (0.084648468721724976)])]
+          #   ]))
+          # ]))
         ];
       };
     };
