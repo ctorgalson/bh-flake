c6086550462c21aff4007eb18adbff5c8e951e56
Christopher Torgalson <manager@bedlamhotel.com>
2024-12-18 14:54:09 -0500

    feat: expands (and alphabetizes) zed editor settings

--- modules/home-manager/programs/zed-editor.nix
+++ modules/home-manager/programs/zed-editor.nix
@@ -18,6 +18,7 @@
         "twig"
       ];
       # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.zed-editor.userKeymaps
+      # https://www.reddit.com/r/ZedEditor/comments/1bd6ene/zed_editor_settings/
       userKeymaps = [
         {
           context = "Editor && vim_mode == normal";
@@ -33,15 +34,55 @@
         }
       ];
       userSettings = {
+        auto_update = false;
+        buffer_font_family = "Inconsolata Nerd Font Mono";
+        buffer_font_size = 16;
+        eslint = {
+          enabled = true;
+          autoFixOnSave = true;
+          autoFixOnFormat = true;
+          autoFixOnFormatDelay = 1500;
+        };
+        font_family = "Inconsolata Nerd Font Mono";
+        font_features = null;
+        font_size = null;
+        formatter = "prettier";
+        git = {
+          enabled = true;
+          git_status = true;
+          git_gutter: "tracked_files";
+        };
+        line_height = "comfortable";
+        linter = "eslint";
+        hour_format = "hour24";
+        node = {
+          npm_path = "${pkgs.nodejs}/bin/npm";
+          path = "${pkgs.nodejs}/bin/node";
+        };
+        preferred_line_length = 80;
+        prettier = {
+          printWidth = 80;
+          semi = true;
+          tabWidth = 2;
+          trailingComma = "all";
+          useTabs = false;
+        };
+        shell = "system";
+        ssh_connections = [
+          {
+            host = "nx";
+            # projects = {
+            #   dcc = "~/Projects/dublin-city-council";
+            # };
+          }
+        ];
+        tabs = {
+          file_icons = true;
+          git_status = true;
+        };
         telemetry = {
           metrics = false;
         };
-        node = {
-          path = "${pkgs.nodejs}/bin/node";
-          npm_path = "${pkgs.nodejs}/bin/npm";
-        };
-        hour_format = "hour24";
-        auto_update = false;
         terminal = {
           alternate_scroll = "off";
           blinking = "off";
@@ -55,30 +96,15 @@
           };
           env = { };
         };
-        font_family = "Inconsolata Nerd Font Mono";
-        font_features = null;
-        font_size = null;
-        line_height = "comfortable";
-        shell = "system";
-        working_directory = "current_project_directory";
-        vim_mode = true;
         theme = {
-          mode = "dark";
-          light = "Solarized Light";
           dark = "Solarized Dark";
+          light = "Solarized Light";
+          mode = "dark";
         };
         ui_font_size = 16;
         ui_font_family = "Inconsolata Nerd Font Mono";
-        buffer_font_size = 16;
-        buffer_font_family = "Inconsolata Nerd Font Mono";
-        ssh_connections = [
-          {
-            host = "nx";
-            # projects = {
-            #   dcc = "~/Projects/dublin-city-council";
-            # };
-          }
-        ];
+        vim_mode = true;
+        working_directory = "current_project_directory";
       };
     };
   };
