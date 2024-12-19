1b69cfe4f8d0876bc276cd02debf474ef71f4ab9
Christopher Torgalson <manager@bedlamhotel.com>
2024-12-19 08:26:35 -0500

    feat: adds a couple more fonts

--- modules/home-manager/fonts/default.nix
+++ modules/home-manager/fonts/default.nix
@@ -4,7 +4,9 @@
   config = {
     fonts.fontconfig.enable = true;
     home.packages = [
+      pkgs.dejavu_fonts
       pkgs.nerd-fonts.inconsolata
+      pkgs.nerd-fonts.ubuntu
       # (pkgs.nerdfonts.override { fonts = [ "Inconsolata" ]; })
     ];
   };
