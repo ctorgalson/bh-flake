323350968fb6e9d4499d8b761e0cdf81793c3763
Christopher Torgalson <manager@bedlamhotel.com>
2024-12-19 08:51:57 -0500

    feat: font-noodling

--- modules/home-manager/fonts/default.nix
+++ modules/home-manager/fonts/default.nix
@@ -5,8 +5,10 @@
     fonts.fontconfig.enable = true;
     home.packages = [
       pkgs.dejavu_fonts
-      pkgs.nerd-fonts.inconsolata
-      pkgs.nerd-fonts.ubuntu
+      pkgs.ubuntu-sans
+      pkgs.nerd-fonts.DejaVuSansMono  
+      pkgs.nerd-fonts.Inconsolata
+      pkgs.nerd-fonts.UbuntuMono
       # (pkgs.nerdfonts.override { fonts = [ "Inconsolata" ]; })
     ];
   };
