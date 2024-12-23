038719146d5298c2c5acff866088ca674a88d8ca
Christopher Torgalson <manager@bedlamhotel.com>
2024-12-23 09:59:13 -0500

    feat: enables night-light setting in gnome settings

--- modules/home-manager/dconf/system.nix
+++ modules/home-manager/dconf/system.nix
@@ -6,6 +6,9 @@
       "org/gnome/system/location" = {
         enabled = true;
       };
+      "org/gnome/settings-daemon/plugins/color" = {
+        night-light-enabled = true;
+      };
     };
   };
 }
