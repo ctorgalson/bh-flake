cd40815070ea01877e17278d67f71cc00146d5ae
Christopher Torgalson <manager@bedlamhotel.com>
2025-01-02 15:35:31 -0500

    feat: adds required treesitter installed, minor neovim tweaks

--- modules/home-manager/programs/nvchad.nix
+++ modules/home-manager/programs/nvchad.nix
@@ -7,8 +7,53 @@
   config = {
     programs.nvchad = {
       enable = true;
+      chadrcConfig= ''
+      ---@type ChadrcConfig
+      local M = {}
+
+      M.base46 = {
+        theme = "catppuccin",
+      }
+
+      return M
+      '';
+      extraConfig = ''
+      vim.o.colorcolumn = '80';
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "bash",
+          "css",
+          "csv",
+          "diff",
+          "gitignore",
+          "graphql",
+          "html",
+          "http",
+          "javascript",
+          "jq",
+          "json",
+          "lua",
+          "luadoc",
+          "markdown",
+          "nginx",
+          "nix",
+          "php",
+          "phpdoc",
+          "python",
+          "sql",
+          "ssh_config",
+          "toml",
+          "tmux",
+          "typescript",
+          "twig",
+          "vim",
+          "vimdoc",
+          "yaml",
+        };
+      })
+      '';
       extraPackages = with pkgs; [ ];
-      backup = true;
+      backup = false;
     };
   };
 }
