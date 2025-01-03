9f73fba732487c11d83510d4e38a82b314f76cf8
Christopher Torgalson <manager@bedlamhotel.com>
2025-01-03 09:42:41 -0500

    feat: adds ensure_installed to custom neovim treesitter config

--- modules/home-manager/programs/neovim/plugins/nvim-treesitter.lua
+++ modules/home-manager/programs/neovim/plugins/nvim-treesitter.lua
@@ -1,5 +1,41 @@
 require('nvim-treesitter.configs').setup({
   highlight = {
     enable = true,
-  }
+    use_languagetree = true,
+  };
+
+  indent = {
+    enable = true,
+  };
+
+  ensure_installed = {
+    "bash",
+    "css",
+    "csv",
+    "diff",
+    "gitignore",
+    "graphql",
+    "html",
+    "http",
+    "javascript",
+    "jq",
+    "json",
+    "lua",
+    "luadoc",
+    "markdown",
+    "nginx",
+    "nix",
+    "php",
+    "phpdoc",
+    "python",
+    "sql",
+    "ssh_config",
+    "toml",
+    "tmux",
+    "typescript",
+    "twig",
+    "vim",
+    "vimdoc",
+    "yaml",
+  },
 })
