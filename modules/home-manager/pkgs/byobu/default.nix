3c2246b4f3c68d1a1e40b22b390cfac51e7ac644
Christopher Torgalson <manager@bedlamhotel.com>
2025-01-04 23:16:11 -0500

    feat: comment out extra tmux config in byobu .tmux.conf
    It doesn't work or only partially works. The unconfigured default
    actually looks better even though it's totally information-free on the
    right side.

--- modules/home-manager/pkgs/byobu/default.nix
+++ modules/home-manager/pkgs/byobu/default.nix
@@ -16,9 +16,30 @@
       # Colour support.
       set -ga terminal-overrides ",xterm-256color:Tc"
 
-      # Enable Catppuccin theme.
-      # run-shell /nix/store/2wg26clcmn7fhi5kf9dfb5a446i9m3zp-tmuxplugin-catppuccin-unstable-2024-05-15/share/tmux-plugins/catppuccin/catppuccin.tmux
+      # Prepare, enable, and configure Catppuccin theme.
+      #
+      # Configure the Catppuccin plugin
+      set -g @catppuccin_flavor "mocha"
+      set -g @catppuccin_window_status_style "rounded"
+      # Load the plugin.
       run-shell ${pkgs.tmuxPlugins.catppuccin}/share/tmux-plugins/catppuccin/catppuccin.tmux
+
+      # # Make the status line pretty and add some modules
+      # set -g status-right-length 100
+      # set -g status-left-length 100
+      # set -g status-left ""
+
+      # set -g status-right '#[fg=#{@thm_crust},bg=#{@thm_teal}] session: #S '
+      # set -agF status-right "#[fg=#{@thm_crust},bg=#{@thm_teal}] ##H "
+
+      # # set -g status-right "#{E:@catppuccin_status_application}"
+      # # set -ag status-right "#{E:@catppuccin_status_session}"
+      # # set -ag status-right "#{E:@catppuccin_status_uptime}"
+
+      # # set -agF status-right "#{E:@catppuccin_status_cpu}"
+      # # set -agF status-right "#{E:@catppuccin_status_battery}"
+      # # run ~/.config/tmux/plugins/tmux-plugins/tmux-cpu/cpu.tmux
+      # # run ~/.config/tmux/plugins/tmux-plugins/tmux-battery/battery.tmux
       '';
     };
   };
