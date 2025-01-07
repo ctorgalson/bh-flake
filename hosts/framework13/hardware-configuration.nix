49d1eb51f8b1252ce9f28949472eb2b78ce97b81
Christopher Torgalson <manager@bedlamhotel.com>
2025-01-06 19:38:32 -0500

    feat: adds hardware config for new framework13 host

---
+++ hosts/framework13/hardware-configuration.nix
@@ -0,0 +1,35 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+  
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod" ];
+
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/4ba3fe0e-3c37-4536-b833-6347a458384b";
+      fsType = "ext4";
+    };
+
+  boot.initrd.luks.devices."luks-7b065af5-f102-448b-be49-8764a79720df".device = "/dev/disk/by-uuid/7b065af5-f102-448b-be49-8764a79720df";
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/BE54-6AC4";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/527f5ab7-8f77-4871-823a-40e20fca7219"; }
+    ];
+
+  networking.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
