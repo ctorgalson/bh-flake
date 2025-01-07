77b1aea385dfa7580dc995b48f8197fd22552a2b
Christopher Torgalson <manager@bedlamhotel.com>
2025-01-06 21:19:48 -0500

    feat: corrects typo: s/services/security

--- hosts/framework13/configuration.nix
+++ hosts/framework13/configuration.nix
@@ -41,7 +41,7 @@
   services.printing.enable = true;
 
   services.pulseaudio.enable = false;
-  services.rtkit.enable = true;
+  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
