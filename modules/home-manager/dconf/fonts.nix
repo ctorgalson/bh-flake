dd0eff61bd198d1a37ee2469951bca4dcde647fb
Christopher Torgalson <manager@bedlamhotel.com>
2025-01-09 20:25:37 -0500

    feat: makes fonts.nix module configurable
    Yet to figure out how to pass vars to it.

--- modules/home-manager/dconf/fonts.nix
+++ modules/home-manager/dconf/fonts.nix
@@ -1,11 +1,37 @@
-{ config, lib, pkgs, programs, ... }:
+{ config, lib, programs, ... }:
 
 {
-  config = {
+  options = {
+    fonts.enable = lib.mkEnableOption "enable dconf fonts module";
+
+    fonts.fontName = lib.mkOption {
+      default = "Cantarell 10";
+      description = ''
+        default interface font and size
+      '';
+    };
+
+    fonts.documentFontName = lib.mkOption {
+      default = "Cantarell 10";
+      description = ''
+        default document font and size
+      '';
+    };
+
+    fonts.monospaceFontName = lib.mkOption {
+      default = "Source Code Pro 10";
+      description = ''
+        default monospace font and size
+      '';
+    };
+  };
+
+  config = lib.mkIf config.fonts.enable {
     dconf.settings = {
       "org/gnome/desktop/interface" = {
-        font-name = "Cantarell 10";
-        document-font-name = "Cantarell 10";
+        font-name = "${config.fonts.fontName}";
+        document-font-name = "${config.fonts.documentFontName}";
+        monospace-font-name = "${config.fonts.monospaceFontName}";
       };
     };
   };
