25b97246b9f13f1c33311b0e98f852d2bd8788a7
Christopher Torgalson <manager@bedlamhotel.com>
2025-01-31 09:08:12 -0500

    feat: adds auto-launch for zellij using the last detached session, if any

--- modules/home-manager/programs/zsh.nix
+++ modules/home-manager/programs/zsh.nix
@@ -5,6 +5,26 @@
     programs.zsh = {
       enable = true;
       enableCompletion = true;
+      envExtra = ''
+        # Searches for existing session and attaches to the first in the list.
+        #
+        # @see https://zellij.dev/documentation/integration.html#autostart-on-shell-creation
+        if [[ -z "$ZELLIJ" ]]; then
+          # Get the first non-EXITED session.
+          last_session="$(zellij list-sessions | grep -v 'EXITED' | head -1 | cut -d' ' -f1)"
+          
+          # If we found one, attach to it. Otherwise, just start Zellij.
+          if [[ -n "$last_session" ]]; then
+            zellij attach "$last_session"
+          else
+            zellij
+          fi
+
+          if [[ "$ZELLIJ_AUTO_EXIT" == "true" ]]; then
+            exit
+          fi
+        fi
+      '';
       plugins = [
         {
           name = "zsh-git-alias";
