1e761d3582cfbee4ad7d2eeba86923045b7416db
Christopher Torgalson <manager@bedlamhotel.com>
2025-02-03 21:22:53 -0500

    feat: correct typo

--- modules/home-manager/dconf/settings-daemon.nix
+++ modules/home-manager/dconf/settings-daemon.nix
@@ -4,8 +4,8 @@
   config = {
     dconf.settings = {
       "org/gnome/settings-daemon/plugins/color" = {
-         night-light-enabled = true;
-       }
+        night-light-enabled = true;
+      };
     };
   };
 }
