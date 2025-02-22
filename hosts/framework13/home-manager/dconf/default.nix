2c6935d948b015710f67bf2d423796d66696cd26
Christopher Torgalson <manager@bedlamhotel.com>
2025-02-22 12:26:21 -0500

    feat: overrides (in theory?) gnome font sizes for desktop role

--- hosts/framework13/home-manager/dconf/default.nix
+++ hosts/framework13/home-manager/dconf/default.nix
@@ -1,9 +1,11 @@
-{ config, lib, programs, ... }:
+{ allowedUnfreePackages, host, inputs, lib, pkgs, system, ... }:
 
 {
-  config.home.dconf.settings = {
-    font-name = "Cantarell 10";
-    document-font-name = "Cantarell 10";
-    monospace-font-name = "Source Code Pro 10";
+  home-manager.users."ctorgalson".config.dconf.settings = {
+    "org.gnome.desktop.interface" = {
+      font-name = "Cantarell 10";
+      document-font-name = "Cantarell 10";
+      monospace-font-name = "Source Code Pro 10";
+    };
   };
 }
