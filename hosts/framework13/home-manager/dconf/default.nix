a9e4b2eee7ef4bed3de95d5c8ccf1c247820c393
Christopher Torgalson <manager@bedlamhotel.com>
2025-02-22 12:10:33 -0500

    feat: different path in set

--- hosts/framework13/home-manager/dconf/default.nix
+++ hosts/framework13/home-manager/dconf/default.nix
@@ -1,7 +1,7 @@
 { config, lib, programs, ... }:
 
 {
-  config.dconf.settings = {
+  config.home.dconf.settings = {
     font-name = "Cantarell 10";
     document-font-name = "Cantarell 10";
     monospace-font-name = "Source Code Pro 10";
