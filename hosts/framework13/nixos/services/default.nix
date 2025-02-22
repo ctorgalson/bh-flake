acb79a16ec5d65815f282a48786ffb341b048404
Christopher Torgalson <manager@bedlamhotel.com>
2025-02-22 11:29:42 -0500

    feat: try explicitly setting driver

--- hosts/framework13/nixos/services/default.nix
+++ hosts/framework13/nixos/services/default.nix
@@ -1,5 +1,9 @@
 { inputs, lib, pkgs, ... }:
 
 {
-  services.fprintd.enable = true;
+  services.fprintd {
+    enable = true;
+    tod.enable = true;
+    tod.driver = pkgs.libfprint-2-tod1-goodix;
+  };
 }
