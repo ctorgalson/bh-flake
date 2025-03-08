9c45a575dd0dabf35d059bb54fc3141dcc21a2db
Christopher Torgalson <manager@bedlamhotel.com>
2025-03-08 09:43:02 -0500

    feat: moves stylix config to own module

--- roles/desktop/nixos/default.nix
+++ roles/desktop/nixos/default.nix
@@ -14,53 +14,7 @@
     ./time
     ./users
     ./virtualisation
+    # From flakes etc.
+    ./stylix
   ];  
-
-  stylix = {
-    enable = true;
-
-    autoEnable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-
-    fonts = {
-      monospace = {
-        name = "UbuntuMono Nerd Font";
-        package = pkgs.nerd-fonts.ubuntu-mono;
-      };
-
-      sizes = {
-        applications = 10;
-        desktop = 10;
-        popups = 8;
-        terminal = 11;
-      };
-    };
-
-    image = ../../../images/IMG_0952.jpg;
-
-    targets = {
-      gnome = {
-        enable = true;
-      };
-
-      grub = {
-        enable = true;
-        useWallpaper = true;
-      };
-
-      nixos-icons = {
-        enable = true;
-      };
-
-      plymouth = {
-        enable = true;
-      };
-    };
-
-    # Tmux (?)
-
-    # Zed
-
-    # Zellij
-  };
 }
---
+++ roles/desktop/nixos/stylix/default.nix
@@ -0,0 +1,51 @@
+{ pkgs, stylix, ... }:
+
+{
+  stylix = {
+    enable = true;
+
+    autoEnable = true;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+
+    fonts = {
+      monospace = {
+        name = "UbuntuMono Nerd Font";
+        package = pkgs.nerd-fonts.ubuntu-mono;
+      };
+
+      sizes = {
+        applications = 10;
+        desktop = 10;
+        popups = 8;
+        terminal = 11;
+      };
+    };
+
+    image = ../../../images/IMG_0952.jpg;
+
+    targets = {
+      gnome = {
+        enable = true;
+      };
+
+      grub = {
+        enable = true;
+        useWallpaper = true;
+      };
+
+      nixos-icons = {
+        enable = true;
+      };
+
+      plymouth = {
+        enable = true;
+      };
+    };
+
+    # Tmux (?)
+
+    # Zed
+
+    # Zellij
+  };
+}
