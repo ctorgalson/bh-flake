7a7c0eacba74f545141c7826607400fc0df05244
Christopher Torgalson <manager@bedlamhotel.com>
2025-03-11 13:51:19 -0400

    feat: removes ssh keys from sops-secrets, enables BW ssh-agent

--- roles/desktop/default.nix
+++ roles/desktop/default.nix
@@ -19,33 +19,33 @@
     secrets = {
       # "example_number" = { };
 
-      "ssh_keys/id_ed25519/private" = {
-        path = "${homedir}/.ssh/id_ed25519";
-        owner = username;
-        group = "users";
-        mode = "600";
-      };
+      # "ssh_keys/id_ed25519/private" = {
+      #   path = "${homedir}/.ssh/id_ed25519";
+      #   owner = username;
+      #   group = "users";
+      #   mode = "600";
+      # };
 
-      "ssh_keys/id_ed25519/public" = {
-        path = "${homedir}/.ssh/id_ed25519.pub";
-        owner = username;
-        group = "users";
-        mode = "644";
-      };
+      # "ssh_keys/id_ed25519/public" = {
+      #   path = "${homedir}/.ssh/id_ed25519.pub";
+      #   owner = username;
+      #   group = "users";
+      #   mode = "644";
+      # };
 
-      "ssh_keys/id_rsa/private" = {
-        path = "${homedir}/.ssh/id_rsa";
-        owner = username;
-        group = "users";
-        mode = "600";
-      };
+      # "ssh_keys/id_rsa/private" = {
+      #   path = "${homedir}/.ssh/id_rsa";
+      #   owner = username;
+      #   group = "users";
+      #   mode = "600";
+      # };
 
-      "ssh_keys/id_rsa/public" = {
-        path = "${homedir}/.ssh/id_rsa.pub";
-        owner = username;
-        group = "users";
-        mode = "644";
-      };
+      # "ssh_keys/id_rsa/public" = {
+      #   path = "${homedir}/.ssh/id_rsa.pub";
+      #   owner = username;
+      #   group = "users";
+      #   mode = "644";
+      # };
     };
   };
 }
--- roles/desktop/nixos/environment/default.nix
+++ roles/desktop/nixos/environment/default.nix
@@ -1,12 +1,28 @@
 { inputs, lib, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    bws
-    docker
-    micro
-    mkcert
-    steam
-    zoom-us
-  ];
+  environment = {
+    sessionVariables = {
+      SSH_AUTH_SOCK = "$HOME.bitwarden-ssh-agent.sock";
+      XDG_CACHE_HOME = "$HOME/.cache";
+      XDG_CONFIG_HOME = "$HOME/.config";
+      XDG_DATA_HOME = "$HOME/.local/share";
+      XDG_STATE_HOME = "$HOME/.local/state";
+
+      # Not officially in the specification
+      XDG_BIN_HOME = "$HOME/.local/bin";
+      PATH = [ 
+        "$''{XDG_BIN_HOME}"
+      ];
+    };
+
+    systemPackages = with pkgs; [
+      bws
+      docker
+      micro
+      mkcert
+      steam
+      zoom-us
+    ];
+  };
 }
