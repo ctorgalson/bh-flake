9cb338ae1008d77740fe6a785748662a498b26e6
Christopher Torgalson <manager@bedlamhotel.com>
2025-07-08 08:25:10 -0400

    feat: adds gco-fzf custom command

---
+++ roles/desktop/home-manager/pkgs/gco-fzf.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    (writeShellScriptBin "gco-fzf" ''
+      if command -v fzf &> /dev/null; then
+        git branch | fzf | xargs git checkout
+      else
+        echo "Could not find 'fzf'. Please install it or use the git command directly."
+      fi
+    '')
+  ];
+}
