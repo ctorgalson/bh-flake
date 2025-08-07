d8e535f33cd4553b794c895774ff57bbf76230c2
Christopher Torgalson <manager@bedlamhotel.com>
2025-08-06 20:19:03 -0400

    feat: removes origin init.sh; renames setup.sh to init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,60 @@
 #!/usr/bin/env bash
 
-nix-shell shell.nix --run "./setup.sh"
+#
+# Updates repo remote url and submodule.
+#
+function updateRepo () {
+  # Static variables for remote name and ssh remote url.
+  remotename="origin"
+  sshremote="git@github.com:ctorgalson/bh-flake.git"
+
+  # Get the actual current remote url.
+  currentremote="$(git remote get-url "$remotename")"
+
+  if [[ $sshremote -ne $currentremote ]]; then
+    # Launch bitwarden.
+    bitwarden &
+
+    # Pause while we login.
+    echo "Press Enter to after authenticating to Bitwarden..."
+    read -r
+
+    # Update the origin url.
+    git remote set-url origin "$sshremote"
+  fi
+
+  # Update the whole repo.
+  git fetch && git pull origin main
+
+  # Update the sops submodule.
+  git submodule update --init --recursive
+}
+
+#
+# Fetches and writes our SOPS key to the file system.
+#
+function getSopsKey () {
+  # Static variables for secret id from BWS, and path to the SOPS age key.
+  secretid="c34b225f-0c4e-42d6-8c2c-b28d0133105f"
+  keypath="$HOME/.config/sops/age/keys.txt.test"
+
+  if [[ ! -f "$keypath" ]]; then
+    # Request a BWS access token.
+    echo -n "Enter BWS_ACCESS_TOKEN..."
+    read -rs bws_access_token
+
+    # Ensure the directory exists.
+    mkdir -p "$(dirname "$keypath")"
+
+    # Retrieve the secret.
+    secret=$(BWS_ACCESS_TOKEN="$bws_access_token" bws secret get "$secretid")
+
+    # Dump the contents of the secret into a file, and set the file's permissions.
+    jq --raw-output .value <<< "$secret" > "$keypath"
+    chmod "644" "$keypath"
+  fi
+}
+
+updateRepo
+
+getSopsKey
