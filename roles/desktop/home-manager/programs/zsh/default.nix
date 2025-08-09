f6ff1dd6b6c025afa1c3b509e892a45b82a91839
Christopher Torgalson <manager@bedlamhotel.com>
2025-08-09 10:35:24 -0400

    feat: temporarily disables zsh-git-alias

--- roles/desktop/home-manager/programs/zsh/default.nix
+++ roles/desktop/home-manager/programs/zsh/default.nix
@@ -16,16 +16,16 @@
         export SSH_AUTH_SOCK="''${HOME}/.bitwarden-ssh-agent.sock"
       '';
       plugins = [
-        {
-          name = "zsh-git-alias";
-          file = "zsh-git-alias.zsh";
-          src = pkgs.fetchFromGitHub {
-            owner = "vanesterik";
-            repo = "zsh-git-alias";
-            rev = "main";
-            sha256 = "sha256-hXk8+Vg6J1X+y0O8+i4cJydKJM//Bob8hrJ7jDW6kBQ=";
-          };
-        }
+        # {
+        #   name = "zsh-git-alias";
+        #   file = "zsh-git-alias.zsh";
+        #   src = pkgs.fetchFromGitHub {
+        #     owner = "vanesterik";
+        #     repo = "zsh-git-alias";
+        #     rev = "main";
+        #     sha256 = "sha256-hXk8+Vg6J1X+y0O8+i4cJydKJM//Bob8hrJ7jDW6kBQ=";
+        #   };
+        # }
       ];
       shellAliases = {
         dig = "doggo";
