5702617aeadc4caf0748e901592e2ba1b2c1d3d3
Christopher Torgalson <manager@bedlamhotel.com>
2025-09-23 15:01:30 -0400

    feat: adds thunderbird, adds back zeal; documents GUI packages

--- roles/desktop/home-manager/pkgs/gui.nix
+++ roles/desktop/home-manager/pkgs/gui.nix
@@ -3,13 +3,12 @@
 {
   config = {
     home.packages = with pkgs; [
-      bitwarden-desktop
+      bitwarden-desktop           # Password manager
       borgbackup
       bruno                       # Open-source IDE For exploring and testing APIs
       # darktable                 # Seems to rely on outdated version of libsoup
-      fragments
-      gimp
-      # gimp-with-plugins
+      fragments                   # Bittorrent client
+      gimp                        # Raser image editor.
       gnomeExtensions.appindicator
       gnomeExtensions.freon
       gnomeExtensions.gsconnect
@@ -20,37 +19,38 @@
       gnome-multi-writer
       gnome-tweaks
       # gnome-extension-manager
-      google-chrome
+      google-chrome               # Chrome browser
       stable-pkgs.gpt4all         # Local LLM runner, but pkg is borked
-      guvcview
+      guvcview                    # Webcam config utility
       libreoffice                 # Office suite
-      nerd-fonts.inconsolata
-      nerd-fonts.ubuntu-mono
-      inkscape-with-extensions
+      nerd-fonts.inconsolata      # Enhanced Inconsolata font
+      nerd-fonts.ubuntu-mono      # Enhanced Ubuntu Mono font
+      inkscape-with-extensions    # Vector image editor
       kdePackages.kruler          # Screen ruler
-      localsend
-      lorem
-      nextcloud-client
-      obsidian
-      stable-pkgs.protonvpn-gui
+      localsend                   # Cross-platform "airdrop" app
+      lorem                       # GUI greeking generator
+      nextcloud-client            # Nextcloud desktop app
+      obsidian                    # Notes app
+      stable-pkgs.protonvpn-gui   # ProtonVPN: appears to be permanently borked
       # quickgui
-      resources
-      signal-desktop-bin
-      slack
+      resources                   # GUI version of "top" etc
+      signal-desktop-bin          # Signal client
+      slack                       # Slack client
       solaar
       spideroak                   # Backups (installed temporarily)
-      stable-pkgs.calibre
+      stable-pkgs.calibre         # Ebook library manager
+      thunderbird                 # Email client
       totem                       # Gnome Videos
       trayscale                   # Unofficial Tailscale GUI
       usbutils
-      via
-      vlc
-      vorta
+      via                         # Keyboard configurator: not sure if working
+      vlc                         # Video player
+      vorta                       # Borg Backup GUI
       xdg-desktop-portal-gnome    # For Zoom screen sharing
       yubioath-flutter
       # yubikey-manager-qt
       # yubikey-personalization-gui
-      # zeal # Temporarily disabled for dep qtwebengine-5.15.19
+      zeal                        # Documentation browser
     ];
   };
 }
