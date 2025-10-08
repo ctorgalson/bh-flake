0025e31d5c2b2f76444cb78682a245bad5ecb8dd
Christopher Torgalson <manager@bedlamhotel.com>
2025-10-08 10:59:53 -0400

    feat: reorganizes gui packages, adds brave, removes gpt4all

--- roles/desktop/home-manager/pkgs/gui.nix
+++ roles/desktop/home-manager/pkgs/gui.nix
@@ -3,12 +3,48 @@
 {
   config = {
     home.packages = with pkgs; [
-      bitwarden-desktop           # Password manager
       borgbackup                  # CLI backup manager (why is this HERE?)
-      bruno                       # Open-source IDE For exploring and testing APIs
       # darktable                 # Seems to rely on outdated version of libsoup
       fragments                   # Bittorrent client
-      gimp                        # Raser image editor.
+      guvcview                    # Webcam config utility
+      ktailctl                    # Unofficial Tailscale GUI (alt to Trayscale)
+      nextcloud-client            # Nextcloud desktop app
+      # quickgui
+      solaar
+      spideroak                   # Backups (installed temporarily)
+      stable-pkgs.calibre         # Ebook library manager
+      # trayscale                   # Unofficial Tailscale GUI
+      usbutils
+      via                         # Keyboard configurator: not sure if working
+      vlc                         # Video player
+      vorta                       # Borg Backup GUI
+      xdg-desktop-portal-gnome    # For Zoom screen sharing
+      yubioath-flutter
+      # yubikey-manager-qt
+      # yubikey-personalization-gui
+
+      # Browsers / mail clients
+      brave                       # chromium-based browser
+      google-chrome               # Chrome browser
+      thunderbird                 # Email client
+
+      # Chat clients / coms
+      localsend                   # Cross-platform "airdrop" app
+      protonvpn-gui               # ProtonVPN: appears to be permanently borked
+      signal-desktop-bin          # Signal client
+      slack                       # Slack client
+
+      # Development tools
+      bruno                       # Open-source IDE For exploring & testing APIs
+      kdePackages.kruler          # Screen ruler
+      resources                   # GUI version of "top" etc
+      zeal                        # Documentation browser
+
+      # Fonts
+      nerd-fonts.inconsolata      # Enhanced Inconsolata font
+      nerd-fonts.ubuntu-mono      # Enhanced Ubuntu Mono font
+
+      # Gnome programs / extensions
       gnomeExtensions.appindicator
       gnomeExtensions.freon
       gnomeExtensions.gsconnect
@@ -19,39 +55,19 @@
       gnome-multi-writer
       gnome-tweaks
       # gnome-extension-manager
-      google-chrome               # Chrome browser
-      stable-pkgs.gpt4all         # Local LLM runner, but pkg is borked
-      guvcview                    # Webcam config utility
-      ktailctl                    # Unofficial Tailscal GUI (alt to Trayscale)
-      libreoffice                 # Office suite
-      nerd-fonts.inconsolata      # Enhanced Inconsolata font
-      nerd-fonts.ubuntu-mono      # Enhanced Ubuntu Mono font
-      inkscape-with-extensions    # Vector image editor
-      kdePackages.kruler          # Screen ruler
-      localsend                   # Cross-platform "airdrop" app
-      lorem                       # GUI greeking generator
-      nextcloud-client            # Nextcloud desktop app
-      obsidian                    # Notes app
-      stable-pkgs.protonvpn-gui   # ProtonVPN: appears to be permanently borked
-      # quickgui
-      resources                   # GUI version of "top" etc
-      signal-desktop-bin          # Signal client
-      slack                       # Slack client
-      solaar
-      spideroak                   # Backups (installed temporarily)
-      stable-pkgs.calibre         # Ebook library manager
-      thunderbird                 # Email client
       totem                       # Gnome Videos
-      # trayscale                   # Unofficial Tailscale GUI
-      usbutils
-      via                         # Keyboard configurator: not sure if working
-      vlc                         # Video player
-      vorta                       # Borg Backup GUI
-      xdg-desktop-portal-gnome    # For Zoom screen sharing
-      yubioath-flutter
-      # yubikey-manager-qt
-      # yubikey-personalization-gui
-      zeal                        # Documentation browser
+
+      # Graphics tools.
+      gimp                        # Raser image editor.
+      inkscape-with-extensions    # Vector image editor
+
+      # Notes/writing tools
+      obsidian                    # Notes app
+      libreoffice                 # Office suite
+      lorem                       # GUI greeking generator
+
+      # Security
+      bitwarden-desktop           # Password manager
     ];
   };
 }
