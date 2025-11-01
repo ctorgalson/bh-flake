b0bce7cea08bd703aa0491366df434daf1830d82
Christopher Torgalson <manager@bedlamhotel.com>
2025-11-01 17:16:17 -0400

    feat: tries another tack with SOPS/wifi

--- roles/desktop/nixos/networking/default.nix
+++ roles/desktop/nixos/networking/default.nix
@@ -1,5 +1,76 @@
-{ config, ... }:
+{ config, lib, ... }:
 
+let
+  # Define Wi-Fi networks here
+  wifiNetworks = [
+    {
+      ssid = "nemo";
+      secretKey = "wifi_psk_nemo";
+      uuid = "a1b2c3d4-5e6f-4a5b-9c8d-1e2f3a4b5c6d";
+    }
+    {
+      ssid = "wensley";
+      secretKey = "wifi_psk_wensley";
+      uuid = "d9e1a8f0-3c4d-4b2a-9e7f-1a2b3c4d5e6f";
+    }
+    {
+      ssid = "wensley_EXT";
+      secretKey = "wifi_psk_wensley_ext";
+      uuid = "e8f2b9c1-4d5e-4c3b-8f6e-2b3c4d5e6f7a";
+    }
+  ];
+
+  # Generate SOPS secrets configuration
+  wifiSecrets = lib.listToAttrs (map (net: {
+    name = net.secretKey;
+    value = {
+      sopsFile = ../../../../sops/secrets.yaml;
+      mode = "0600";
+      owner = "root";
+    };
+  }) wifiNetworks);
+
+  # Generate sops templates
+  wifiTemplates = lib.listToAttrs (map (net: {
+    name = "${net.ssid}.nmconnection";
+    value = {
+      content = ''
+        [connection]
+        id=${net.ssid}
+        uuid=${net.uuid}
+        type=wifi
+        autoconnect=false
+
+        [wifi]
+        mode=infrastructure
+        ssid=${net.ssid}
+
+        [wifi-security]
+        key-mgmt=wpa-psk
+        psk=''${config.sops.placeholder.${net.secretKey}}
+
+        [ipv4]
+        method=auto
+
+        [ipv6]
+        method=auto
+      '';
+      mode = "0600";
+      owner = "root";
+      group = "root";
+    };
+  }) wifiNetworks);
+
+  # Generate environment.etc entries
+  wifiEtcFiles = lib.listToAttrs (map (net: {
+    name = "NetworkManager/system-connections/${net.ssid}.nmconnection";
+    value = {
+      source = config.sops.templates."${net.ssid}.nmconnection".path;
+      mode = "0600";
+    };
+  }) wifiNetworks);
+
+in
 {
   networking.firewall = {
     allowedTCPPorts = [
@@ -26,80 +97,11 @@
   networking.networkmanager.enable = true;
 
   # SOPS secrets for Wi-Fi passwords
-  sops = {
-    secrets = {
-      wifi_psk_nemo = {
-        sopsFile = ../../../../sops/secrets.yaml;
-        mode = "0600";
-        owner = "root";
-      };
-      wifi_psk_wensley = {
-        sopsFile = ../../../../sops/secrets.yaml;
-        mode = "0600";
-        owner = "root";
-      };
-      wifi_psk_wensley_ext = {
-        sopsFile = ../../../../sops/secrets.yaml;
-        mode = "0600";
-        owner = "root";
-      };
-    };
-  };
+  sops.secrets = wifiSecrets;
 
   # Generate Network Manager connection files with SOPS-managed passwords
-  sops.templates."wensley.nmconnection" = {
-    content = ''
-      [connection]
-      id=wensley
-      uuid=d9e1a8f0-3c4d-4b2a-9e7f-1a2b3c4d5e6f
-      type=wifi
-      autoconnect=false
+  sops.templates = wifiTemplates;
 
-      [wifi]
-      mode=infrastructure
-      ssid=wensley
-
-      [wifi-security]
-      key-mgmt=wpa-psk
-      psk=${config.sops.placeholder.wifi_psk_wensley}
-
-      [ipv4]
-      method=auto
-
-      [ipv6]
-      method=auto
-    '';
-    path = "/etc/NetworkManager/system-connections/wensley.nmconnection";
-    mode = "0600";
-    owner = "root";
-    group = "root";
-  };
-
-  sops.templates."wensley_EXT.nmconnection" = {
-    content = ''
-      [connection]
-      id=wensley_EXT
-      uuid=e8f2b9c1-4d5e-4c3b-8f6e-2b3c4d5e6f7a
-      type=wifi
-      autoconnect=false
-
-      [wifi]
-      mode=infrastructure
-      ssid=wensley_EXT
-
-      [wifi-security]
-      key-mgmt=wpa-psk
-      psk=${config.sops.placeholder.wifi_psk_wensley_ext}
-
-      [ipv4]
-      method=auto
-
-      [ipv6]
-      method=auto
-    '';
-    path = "/etc/NetworkManager/system-connections/wensley_EXT.nmconnection";
-    mode = "0600";
-    owner = "root";
-    group = "root";
-  };
+  # Symlink generated files to NetworkManager's directory
+  environment.etc = wifiEtcFiles;
 }
