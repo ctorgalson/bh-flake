35c2e061e413248188965791f8c9c4fd2ee99939
Christopher Torgalson <manager@bedlamhotel.com>
2025-11-01 16:25:25 -0400

    feat: attempt adding wifi connections via nix/sops

--- roles/desktop/nixos/networking/default.nix
+++ roles/desktop/nixos/networking/default.nix
@@ -24,4 +24,74 @@
   };
 
   networking.networkmanager.enable = true;
+
+  # SOPS secrets for Wi-Fi passwords
+  sops.secrets.wifi_psk_wensley = {
+    sopsFile = ../../../../sops/secrets.yaml;
+    mode = "0600";
+    owner = "root";
+  };
+
+  sops.secrets.wifi_psk_wensley_ext = {
+    sopsFile = ../../../../sops/secrets.yaml;
+    mode = "0600";
+    owner = "root";
+  };
+
+  # Generate Network Manager connection files with SOPS-managed passwords
+  sops.templates."wensley.nmconnection" = {
+    content = ''
+      [connection]
+      id=wensley
+      uuid=d9e1a8f0-3c4d-4b2a-9e7f-1a2b3c4d5e6f
+      type=wifi
+      autoconnect=false
+
+      [wifi]
+      mode=infrastructure
+      ssid=wensley
+
+      [wifi-security]
+      key-mgmt=wpa-psk
+      psk=${config.sops.placeholder.wifi_psk_wensley}
+
+      [ipv4]
+      method=auto
+
+      [ipv6]
+      method=auto
+    '';
+    path = "/etc/NetworkManager/system-connections/wensley.nmconnection";
+    mode = "0600";
+    owner = "root";
+    group = "root";
+  };
+
+  sops.templates."wensley_EXT.nmconnection" = {
+    content = ''
+      [connection]
+      id=wensley_EXT
+      uuid=e8f2b9c1-4d5e-4c3b-8f6e-2b3c4d5e6f7a
+      type=wifi
+      autoconnect=false
+
+      [wifi]
+      mode=infrastructure
+      ssid=wensley_EXT
+
+      [wifi-security]
+      key-mgmt=wpa-psk
+      psk=${config.sops.placeholder.wifi_psk_wensley_ext}
+
+      [ipv4]
+      method=auto
+
+      [ipv6]
+      method=auto
+    '';
+    path = "/etc/NetworkManager/system-connections/wensley_EXT.nmconnection";
+    mode = "0600";
+    owner = "root";
+    group = "root";
+  };
 }
