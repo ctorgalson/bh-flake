b07164246e1369190219c99ce58cfe83728206d7
Christopher Torgalson <manager@bedlamhotel.com>
2025-12-17 19:59:26 -0500

    feat: further tweaking

--- roles/desktop/home-manager/pkgs/wakehost.nix
+++ roles/desktop/home-manager/pkgs/wakehost.nix
@@ -28,7 +28,7 @@
         esac
 
         echo "Sending WakeOnLAN packet to $HOSTNAME ($MAC_ADDRESS) via pi0..."
-        tailscale ssh pi0 "wakeonlan $MAC_ADDRESS"
+        ssh pi0 "wakeonlan $MAC_ADDRESS"
 
         if [ $? -eq 0 ]; then
           echo "WakeOnLAN packet sent successfully"
