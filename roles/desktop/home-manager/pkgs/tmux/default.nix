b210b18f80edc6f7abb0dead113976e2057ad056
Christopher Torgalson <manager@bedlamhotel.com>
2026-01-04 22:25:01 -0500

    feat: tweak tmux config some more

--- roles/desktop/home-manager/pkgs/tmux/default.nix
+++ roles/desktop/home-manager/pkgs/tmux/default.nix
@@ -55,15 +55,15 @@
       # Catppuccin Configuration
       set -g @catppuccin_flavor "mocha"
       set -g @catppuccin_window_status_style "rounded"
-      set -g @catppuccin_window_default_text "#W"              # Inactive: just name
-      set -g @catppuccin_window_current_text "#{host_short}: #W"  # Active: hostname: name
+      set -g @catppuccin_window_default_text " #W"              # Inactive: just name
+      set -g @catppuccin_window_current_text " #{host_short}: #W"  # Active: hostname: name
 
       # Status Bar
       set -g status-right-length 100
       set -g status-left-length 100
       set -g status-left ""
-      set -agF status-right "#{E:@catppuccin_status_cpu}"
-      set -ag status-right "#{E:@catppuccin_status_load}"
+      set -gF status-right "#{E:@catppuccin_status_cpu}"
+      set -agF status-right "#{E:@catppuccin_status_load}"
       set -ag status-right "#{E:@catppuccin_status_uptime}"
       set -ag status-right "#{E:@catppuccin_status_date_time}"
 
