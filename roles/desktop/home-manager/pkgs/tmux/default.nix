ea120caf773211fc85a9ac9a8eff33ad6083e40e
Christopher Torgalson <manager@bedlamhotel.com>
2026-01-05 22:18:56 -0500

    feat: fixes typo

--- roles/desktop/home-manager/pkgs/tmux/default.nix
+++ roles/desktop/home-manager/pkgs/tmux/default.nix
@@ -67,7 +67,7 @@
       set -g status-right-length 100
       set -g status-left-length 100
       set -g status-left ""
-      set -g @catppuccin_status_module_bg_color "#1E1E2E
+      set -g @catppuccin_status_module_bg_color "#1E1E2E"
 
       # Pane Borders
       set -g pane-active-border-style "fg=#fe640b,bg=#151515"
