58db018b52229602532a035dc2c5b58fd5ef98fd
Christopher Torgalson <manager@bedlamhotel.com>
2026-01-12 09:11:27 -0500

    feat: adds policies to firefox config

--- roles/desktop/home-manager/programs/firefox.nix
+++ roles/desktop/home-manager/programs/firefox.nix
@@ -48,6 +48,70 @@
       #  #   };
       #  # };
       #};
+
+      policies = {
+        BlockAboutConfig = true;
+
+        DefaultDownloadDirectory = "\${home}/Downloads";
+
+        ExtensionSettings = {
+          # 1password.
+          "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
+            installation_mode = "force_installed";
+          };
+
+          # AXE Devtools.
+          "@axe-firefox-devtools" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/axe-devtools/latest.xpi";
+            installation_mode = "force_installed";
+          };
+
+          # Bitwarden.
+          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+            installation_mode = "force_installed";
+          };
+
+          # Catppuccin Latte - Blue.
+          #
+          # TODO: do this in a way that works...
+          "{68f3538d-3881-45f4-aa73-288b010b39a1}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/catppuccin-latte-blue-theme/latest.xpi";
+            installation_mode = "force_installed";
+          };
+
+          # Catppuccin Mocha - Blue.
+          #
+          # TODO: do this in a way that works...
+          "{2adf0361-e6d8-4b74-b3bc-3f450e8ebb69}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/catppuccin-mocha-blue-theme/latest.xpi";
+            installation_mode = "force_installed";
+          };
+
+          # Firefox Multi-Account Containers.
+          "@testpilot-containers" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/multi-account-containers/latest.xpi";
+            installation_mode = "force_installed";
+          };
+
+          # Obsidian Web Clipper.
+          "clipper@obsidian.md" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/web-clipper-obsidian/latest.xpi";
+            installation_mode = "force_installed";
+          };
+
+          # uBlock Origin
+          "uBlock0@raymondhill.net" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+            installation_mode = "force_installed";
+            private_browsing = true;
+          };
+        };
+
+        OfferToSaveLogins = false;
+      };
+
       profiles = {
         newwork = {
           containers = {
