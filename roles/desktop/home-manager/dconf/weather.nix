3c95c4a7f8ce133099c4f085393d00eb7f05757d
Christopher Torgalson <manager@bedlamhotel.com>
2026-01-20 08:14:52 -0500

    feat: adds Victoria, makes code reusable

--- roles/desktop/home-manager/dconf/weather.nix
+++ roles/desktop/home-manager/dconf/weather.nix
@@ -1,61 +1,53 @@
 { config, lib, pkgs, programs, ... }:
 
+let
+  mkLocation = { name, airport, lat, lon }:
+    lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
+      (lib.hm.gvariant.mkUint32 2)
+      (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
+        name
+        airport
+        true
+        [(lib.hm.gvariant.mkTuple [lat lon])]
+        [(lib.hm.gvariant.mkTuple [lat lon])]
+      ]))
+    ]);
+
+  toronto = mkLocation {
+    name = "Toronto";
+    airport = "CYTZ";
+    lat = 0.76154532446909495;
+    lon = -1.3857914260834978;
+  };
+
+  victoria = mkLocation {
+    name = "Victoria";
+    airport = "CYYJ";
+    lat = 0.84476620253905869;
+    lon = -2.1532672843410971;
+  };
+
+  # lyon = mkLocation {
+  #   name = "Lyon";
+  #   airport = "LFLY";
+  #   lat = 0.7979063621878385;
+  #   lon = 0.086393797973719322;
+  # };
+
+  locations = [
+    toronto
+    victoria
+    # lyon
+  ];
+in
 {
   config = {
     dconf.settings = {
       "org/gnome/Weather" = {
-        "locations" = [
-          # Toronto
-          (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
-            (lib.hm.gvariant.mkUint32 2)
-            (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
-              "Toronto"
-              "CYTZ"
-              true
-              [(lib.hm.gvariant.mkTuple [(0.76154532446909495) (-1.3857914260834978)])]
-              [(lib.hm.gvariant.mkTuple [(0.76212711252195475) (-1.3860823201099277)])]
-            ]))
-          ]))
-
-          # Lyon
-          # (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
-          #   (lib.hm.gvariant.mkUint32 2)
-          #   (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
-          #     "Lyon"
-          #     "LFLY"
-          #     true
-          #     [(lib.hm.gvariant.mkTuple [(0.7979063621878385) (0.086393797973719322)])]
-          #     [(lib.hm.gvariant.mkTuple [(0.79848813278740571) (0.084648468721724976)])]
-          #   ]))
-          # ]))
-        ];
+        "locations" = locations;
       };
       "org/gnome/shell/weather" = {
-        "locations" = [
-          # Toronto
-          (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
-            (lib.hm.gvariant.mkUint32 2)
-            (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
-              "Toronto"
-              "CYTZ"
-              true
-              [(lib.hm.gvariant.mkTuple [(0.76154532446909495) (-1.3857914260834978)])]
-              [(lib.hm.gvariant.mkTuple [(0.76212711252195475) (-1.3860823201099277)])]
-            ]))
-          ]))
-
-          # Lyon
-          # (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
-          #   (lib.hm.gvariant.mkUint32 2)
-          #   (lib.hm.gvariant.mkVariant (lib.hm.gvariant.mkTuple [
-          #     "Lyon"
-          #     "LFLY"
-          #     true
-          #     [(lib.hm.gvariant.mkTuple [(0.7979063621878385) (0.086393797973719322)])]
-          #     [(lib.hm.gvariant.mkTuple [(0.79848813278740571) (0.084648468721724976)])]
-          #   ]))
-          # ]))
-        ];
+        "locations" = locations;
       };
     };
   };
