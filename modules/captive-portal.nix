a32a58ded5d50943ea3f2f4f87f3ba0c37a963f0
Christopher Torgalson <manager@bedlamhotel.com>
2026-01-24 18:38:54 -0500

    feat: actually COMMIT newly created modules

---
+++ modules/captive-portal.nix
@@ -0,0 +1,31 @@
+{ ... }:
+
+{
+  # Captive portal support for laptops
+  # Enables automatic detection and authentication with captive portals
+  # commonly found in coffee shops, hotels, airports, etc.
+
+  networking = {
+    firewall = {
+      allowedUDPPorts = [
+        53 # DNS - required for captive portal detection
+      ];
+
+      allowedTCPPorts = [
+        53 # DNS over TCP
+      ];
+
+      # Allows asymmetric routing (common with captive portals)
+      checkReversePath = "loose";
+    };
+
+    networkmanager = {
+      extraConfig = ''
+        [connectivity]
+        enabled = true
+        uri = http://nmcheck.gnome.org/check_network_status.txt
+        interval = 300
+      '';
+    };
+  };
+}
